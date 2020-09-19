277703cb7f3fd4c2d804e75f41f8ecc38945b72d
Swapnil Patil <patilswapnilm007@gmail.com>
2020-09-19 20:10:20 +0530

    Updates

---
+++ precache-manifest.de135c19170186f6f5ceb5b1748f2079.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "195146abe840d3f535a98db81fe92345",
+    "url": "/patilswapnilm007/flight_search_engine/index.html"
+  },
+  {
+    "revision": "dae4206473a337f89ac1",
+    "url": "/patilswapnilm007/flight_search_engine/static/css/main.4dfd47fc.chunk.css"
+  },
+  {
+    "revision": "c0bd6f4a1c5fe0527495",
+    "url": "/patilswapnilm007/flight_search_engine/static/js/2.0fc33ad3.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/patilswapnilm007/flight_search_engine/static/js/2.0fc33ad3.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "dae4206473a337f89ac1",
+    "url": "/patilswapnilm007/flight_search_engine/static/js/main.d20d48ff.chunk.js"
+  },
+  {
+    "revision": "67004f0b2499d18b19f2",
+    "url": "/patilswapnilm007/flight_search_engine/static/js/runtime-main.2ae0e62c.js"
+  }
+]);
